27b00eea15d8b7419b6dd518b93d5bf1ec552703
YousifMASC6git config --global user.name YousifMASC6git config --global user.name YousifMASC6 <Yousifmoftah@gmail.com>
2019-07-11 13:45:52 -0400

    RNG

---
+++ RandomNumberTimes.js
@@ -0,0 +1,10 @@
+const multiplier = 100;
+
+function rand(word){
+    const decimal = Math.random();
+    const times = decimal * multiplier;
+    const final = Math.floor(times)
+    oncompassneedscalibration
+    return final + word;
+}
+console.log(rand(" times"));
